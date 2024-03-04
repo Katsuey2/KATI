9c4d95c49f080c167fd902aa1c4fb927315cf73e
Katsuey2 <162149209+Katsuey2@users.noreply.github.com>
2024-03-05 00:25:06 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,12 @@
+
+let Yes= document.getElementById('Ty')
+
+
+Yes.onclick = function(){
+  thankyou = document.getElementById('para2').textContent 
+  = "jdjfjgmtntktkgkgkgkgigihigkgigk"
+  
+}
+
+
+
